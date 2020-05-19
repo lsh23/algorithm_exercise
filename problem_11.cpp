e9ec1ce562eecd69fd60d14911853dc160481cc6
SehyeongLee <shseoul14@gmail.com>
2020-05-18 23:15:01 -0700

    Solve problem_11 on other way

--- problem_11.cpp
+++ problem_11.cpp
@@ -6,11 +6,11 @@ int main(void){
     cin >> n;
     int answer = 0;
     for(i=1;i<=n;i++){
-        if(i<10) answer +=1;
-        else if(i<100) answer +=2;
-        else if(i<1000) answer +=3;
-        else if(i<10000) answer +=4;
-        else if(i<100000) answer +=5;
+        int tmp = i;
+        while(tmp>0){
+            tmp = tmp/10;
+            answer++;
+        }
     }
     cout << answer;
     return 0;
